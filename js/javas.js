db205499ae9b60f80adf2fe38c5e287426eb88e0
Marcelo3duardo <fmarcelo3duardo@gmail.com>
2021-12-14 10:03:43 -0300

    slide depoimentos

--- js/javas.js
+++ js/javas.js
@@ -1,9 +1,42 @@
 /*$(window).on('load', function(){
     alert("Carregou!!");
 })
-
+*/
 $(document).ready(function () {
-    alert('Deu certo');
-    });*/
 
-   
+    $('.mobile').click(function () {
+        $(this).find('ul').slideToggle();
+    })
+    // alert('Deu certo');
+    iniciarDepoimentos();
+    navDepoimentos();
+    var curIndex = 0;
+    var amtDepoimento = $('.depoimento-single p').length;
+    function iniciarDepoimentos() {
+        $('.depoimento-single p').hide();
+        $('.depoimento-single p').eq(0).show();
+    }
+
+    function navDepoimentos() {
+        $('[next]').click(function () {
+            // console.log('next');
+            curIndex++;
+            if (curIndex >= amtDepoimento)
+                curIndex = 0;
+            $('.depoimento-single p').hide();
+            $('.depoimento-single p').eq(curIndex).show();
+        })
+        $('[prev]').click(function () {
+           // console.log('prev');
+           curIndex--;
+           if (curIndex < 0)
+               curIndex = amtDepoimento - 1;
+           $('.depoimento-single p').hide();
+           $('.depoimento-single p').eq(curIndex).show();
+        })
+
+
+    }
+
+});
+
